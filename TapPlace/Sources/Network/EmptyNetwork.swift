8814cb400bb484649cc6e6e6a38714ecab019616
ParkSangHyeon <ios.humhae@gmail.com>
2022-08-03 23:03:13 +0900

    init: Pod Setting

---
+++ TapPlace/Sources/Network/EmptyNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyNetwork.swift
+//  TapPlace
+//
+//  Created by 박상현 on 2022/08/03.
+//
+
+import Foundation
---
+++ TapPlace/Sources/ViewModels/EmptyViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyViewModel.swift
+//  TapPlace
+//
+//  Created by 박상현 on 2022/08/03.
+//
+
+import Foundation

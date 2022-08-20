fed6c71858c448a0cbad6c4432281c8a9796640f
ParkSangHyeon <ios.humhae@gmail.com>
2022-08-20 15:51:14 +0900

    add: AroundPlaceListView.swift

---
+++ TapPlace/Sources/Views/Main/AroundPlace/AroundPlaceListView.swift
@@ -0,0 +1,20 @@
+//
+//  AroundPlaceListView.swift
+//  TapPlace
+//
+//  Created by 박상현 on 2022/08/20.
+//
+
+import UIKit
+
+class AroundPlaceListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

75bfa5282d86b4dd5c8d7febbecd63e7f1fd911f
sangjun <jesusfollow1103@naver.com>
2022-08-25 11:11:12 +0900

    no message

---
+++ TapPlace/Sources/ViewControllers/Intro/PrivacyViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PrivacyViewController.swift
+//  TapPlace
+//
+//  Created by 이상준 on 2022/08/24.
+//
+
+import Foundation
+import UIKit
+
+class PrivacyViewController: UIViewController {
+    
+    
+}

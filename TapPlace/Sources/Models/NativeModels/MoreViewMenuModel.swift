c544199a99d8d1724fba29b77826fbbf7fe117de
ParkSangHyeon <ios.humhae@gmail.com>
2022-09-10 11:40:15 +0900

    fix: 더보기 VC 메뉴 선택 로직 구현(진행중)

--- TapPlace/Sources/Models/NativeModels/MoreViewMenuModel.swift
+++ TapPlace/Sources/Models/NativeModels/MoreViewMenuModel.swift
@@ -23,7 +23,7 @@ extension MoreMenuModel {
     static let list: [MoreMenuModel] = [
         MoreMenuModel(title: "공지사항", type: .notice, subTitle: nil, vc: NoticeViewController()),
         MoreMenuModel(title: "버전정보", type: .version, subTitle: .version, vc: nil),
-        MoreMenuModel(title: "자주 묻는 질문", type: .faq, subTitle: nil, vc: nil),
+        MoreMenuModel(title: "자주 묻는 질문", type: .faq, subTitle: nil, vc: FAQViewController()),
         MoreMenuModel(title: "문의하기", type: .qna, subTitle: nil, vc: InquiryViewController()),
         MoreMenuModel(title: "수정제안", type: .request, subTitle: nil, vc: InquiryViewController()),
         MoreMenuModel(title: "활동내역 초기화", type: .reset, subTitle: nil, vc: nil)

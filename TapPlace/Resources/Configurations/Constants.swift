ff8351d84c310cc20caa35a4a6f4bcb4a6d5ac72
ParkSangHyeon <ios.humhae@gmail.com>
2022-09-14 19:40:51 +0900

    fix: 약관 웹뷰 뷰포트 추가 및 탭플레이스 URL 변경

--- TapPlace/Resources/Configurations/Constants.swift
+++ TapPlace/Resources/Configurations/Constants.swift
@@ -11,7 +11,7 @@ struct Constants {
     static let userDeviceID = CommonUtils.getDeviceUUID()
     static let naverClientId = Bundle.main.infoDictionary?["NAVER_CLIENT_ID"] as? String
     static let kakaoRestApiKey = Bundle.main.infoDictionary?["KAKAO_REST_API_KEY"] as? String
-    static let tapplaceBaseUrl = "https://tapplace.co.kr"
+    static let tapplaceBaseUrl = "https://www.tapplace.co.kr"
     static let tapplaceApiUrl = "https://api.tapplace.cloud"
     static let tapplaceApiKey = Bundle.main.infoDictionary?["TAPPLACE_API_KEY"] as? String ?? ""
     static let tapplaceConsentUrl = tapplaceBaseUrl + "/consent"

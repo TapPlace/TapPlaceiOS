64b46fc9f2e17d2a7c7763706ea38e93a4eb54f6
ParkSangHyeon <ios.humhae@gmail.com>
2022-09-16 00:07:15 +0900

    fix: 빌드 환경 분리 (디버그, 프로덕션) 개발시에 무조건 Debug 빌드!!!

--- TapPlace/Sources/ViewControllers/Intro/SplashViewController.swift
+++ TapPlace/Sources/ViewControllers/Intro/SplashViewController.swift
@@ -13,7 +13,7 @@ class SplashViewController: UIViewController {
     //MARK: - ViewController Lift Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print(storageViewModel.dataBases?.location)
+        print("TAPPLACE_API_URL: \(Constants.tapplaceApiUrl)")
         print(Constants.userDeviceID)
         setupView()
         userInfoSetting()
